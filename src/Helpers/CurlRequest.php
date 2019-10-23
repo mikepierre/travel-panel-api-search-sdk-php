697cc3d21dab357c8bc8a2764bdb6a58fe060ccc
mikepierre <mikepierredotcom@gmail.com>
2019-10-23 11:48:09 -0400

    Refectored
    Refactored Curl Request.

--- src/Helpers/CurlRequest.php
+++ src/Helpers/CurlRequest.php
@@ -7,14 +7,22 @@ namespace TravelPanel\Helpers;
 
 class CurlRequest
 {
+	/**
+	* Send Request to HTTP.
+	* @param array of data to build response
+	* @return connection response & its data
+	**/
 	protected function sendRequest($data)
 	{
+		// Build the endpoint
+		$endpoint = $data['url'].$data['params'].'&'.$data['auth'];
+
         try {
 	        $ch = curl_init();
 	        // Will return the response, if false it print the response
 	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	        // Set the url
-	        curl_setopt($ch, CURLOPT_URL,$transferUrl );
+	        curl_setopt($ch, CURLOPT_URL,$endpoint );
 	        // Execute
 	        $response=curl_exec($ch);
 	        // Closing
